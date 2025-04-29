539aba597210e86071d35f7de1b9aa3f6d8414c6
kbforsure <katieannablair@gmail.com>
2025-04-28 18:20:08 -0700

    trying to fix my event listener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
-
 function generatePoem(event) {
   event.preventDefault();
   alert("Generating your poem...");
 }
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
